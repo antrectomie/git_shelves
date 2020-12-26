80760a36a2cd4a8e0336daad9571b0f7e0a73d23
Budes Alin <alin76alin@yahoo.com>
2020-12-26 17:05:00 +0200

    created person

---
+++ src/main/java/com/example/git_shelves/model/Person.java
@@ -0,0 +1,22 @@
+package com.example.git_shelves.model;
+
+public class Person {
+    private String test;
+    private Long id;
+
+    public String getTest() {
+        return test;
+    }
+
+    public void setTest(String test) {
+        this.test = test;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
