86669cb8bf89a690ae0b41bc1c8bdacf5ae6a40a
Budes Alin <alin76alin@yahoo.com>
2020-12-26 16:38:06 +0200

    first commit

---
+++ src/test/java/com/example/git_shelves/GitShelvesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.git_shelves;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitShelvesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
